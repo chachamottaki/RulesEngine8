4f41a39fee1b01721529ec3d850dede68101d15b
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2024-05-08 10:23:18 +0200

    added alarmId

--- RulesEngine8/Models/HistoryTable.cs
+++ RulesEngine8/Models/HistoryTable.cs
@@ -3,7 +3,11 @@
     public class HistoryTable
     {
         public int Id { get; set; }
-        public string? assetUUID { get; set; }
+
+        //public string? assetUUID { get; set; }
+        public bool isAlarm { get; set; }
+        public string alarmId { get; set; }
+        public string DeviceId { get; set; }
         public bool emailSent { get; set; }
         public string? emailContent { get; set; }
         public string? emailRecipient{ get; set; }

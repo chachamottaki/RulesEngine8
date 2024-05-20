659af9f2dfc758b451593a7df4c3bd40c570542f
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2024-05-20 09:14:50 +0200

    removed unnecessary comments

--- RulesEngine8/Controllers/SensorController.cs
+++ RulesEngine8/Controllers/SensorController.cs
@@ -24,26 +24,6 @@ namespace RulesEngine8.Controllers
 
         }
 
-        // GET: api/<SensorController>
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-            //return new string[] { "value1", "value2" };
-        //}
-
-        // GET api/<SensorController>/5
-        /*[HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }*/
-
-        // POST api/<SensorController>
-        /*[HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }*/
-
         // POST api/<ConfigController>
         [HttpPost("districts/{district}/installations/{assetType}/{assetKey}/sensors/{sensorKey}:{sensorType}")]
         public async Task<IActionResult> Post(
@@ -113,17 +93,5 @@ namespace RulesEngine8.Controllers
             }
             return Ok(new { Hostname = hostname, District = district, AssetType = assetType, AssetKey = assetKey, SensorKey = sensorKey, SensorType = sensorType, RequestBody = sensor });
         }
-
-        // PUT api/<SensorController>/5
-        /*[HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }*/
-
-        // DELETE api/<SensorController>/5
-        /*[HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }*/
     }
 }

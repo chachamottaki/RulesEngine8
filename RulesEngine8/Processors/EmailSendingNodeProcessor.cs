095fe7bc7f08677b58a06ec9d18c013034598780
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2024-05-23 09:40:59 +0200

    added emailSending node

--- RulesEngine8/Processors/EmailSendingNodeProcessor.cs
+++ RulesEngine8/Processors/EmailSendingNodeProcessor.cs
@@ -2,46 +2,73 @@
 using RulesEngine8.Services;
 using System.Text.Json.Nodes;
 using RulesEngine8.Models;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace RulesEngine8.Processors
 {
     public class EmailSendingNodeProcessor : IRuleNodeProcessor
     {
         private readonly IEmailService _emailService;
+        private readonly RulesEngineDBContext _context;
 
-        public EmailSendingNodeProcessor(IEmailService emailService)
+        public EmailSendingNodeProcessor(IEmailService emailService, RulesEngineDBContext context)
         {
-            _emailService = emailService;
+            //_emailService = emailService;
+            _context = context;
         }
 
         public string NodeType => "EmailSending";
 
         public async Task ProcessAsync(RuleNode node, RuleExecutionContext context)
         {
+            var configItem = (ConfigItem)context.State["ConfigItem"];
+            var alarm = (DI)context.State["Alarm"];
+            System.Diagnostics.Debug.WriteLine("IN EMAILSENDING NODE");
             if (context.State.ContainsKey("EmailRecipient") && context.State.ContainsKey("EmailContent"))
             {
                 var recipient = context.State["EmailRecipient"].ToString();
                 var content = context.State["EmailContent"].ToString();
-
-                // Send the email
-                await _emailService.SendEmailAsync(recipient, "Alarm Triggered", content);
-
-                // Save history record
-                var historyRecord = new HistoryTable
+                bool shouldSendEmail = (context.State.ContainsKey("sendEmail") && (bool)context.State["sendEmail"]) &&
+                                       (!context.State.ContainsKey("invertSendEmail") || !(bool)context.State["invertSendEmail"]);
+                if (shouldSendEmail)
                 {
-                    isAlarm = ((DI)context.State["Alarm"]).IsAlarm,
-                    alarmId = context.InputData["alarmId"].GetValue<string>(),
-                    assetId = context.InputData["assetKey"].GetValue<string>(),
-                    DeviceId = context.InputData["hostname"].GetValue<string>(),
-                    emailSent = true,
-                    emailRecipient = recipient,
-                    emailContent = content,
-                    timestamp = DateTime.Now
-                };
+                    var historyRecord = new HistoryTable
+                    {
+                        isAlarm = alarm.IsAlarm,
+                        alarmId = alarm.alarmId,
+                        assetId = alarm.InstallationKey,
+                        DeviceId = configItem.DeviceID,
+                        emailSent = true,
+                        emailRecipient = recipient,
+                        emailContent = (string)context.State["EmailContent"],
+                        timestamp = DateTime.Now
+                    };
+                    _context.HistoryTables.Add(historyRecord);
+                    System.Diagnostics.Debug.WriteLine("ADD HISTORY RECORD FALSE");
+                    await _context.SaveChangesAsync();
 
-                context.State["HistoryRecord"] = historyRecord;
+                    // Send the email
+                    //await _emailService.SendEmailAsync(recipient, "Alarm Triggered", content);
+                }
+                else
+                {
+                    // Save history record
+                    var historyRecord = new HistoryTable
+                    {
+                        isAlarm = alarm.IsAlarm,
+                        alarmId = alarm.alarmId,
+                        assetId = alarm.InstallationKey,
+                        emailSent = true,
+                        emailRecipient = recipient,
+                        emailContent = content,
+                        timestamp = DateTime.Now
+                    };
+                    _context.HistoryTables.Add(historyRecord);
+                    System.Diagnostics.Debug.WriteLine("ADD HISTORY RECORD FALSE");
+                    await _context.SaveChangesAsync();   
+                }
             }
-
             await Task.CompletedTask;
         }
     }
